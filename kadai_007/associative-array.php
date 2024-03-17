99317bc802859b05932b28a7fe7b3916a7d8321a
kirakara-llOl <goto.kn.my@gmail.com>
2024-03-17 14:01:18 +0900

    kadai_007ディレクトリの追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>【課題】連想配列を作って値とキーを出力しよう</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        // 連想配列に値を代入する
+        $product_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        // 連想配列の値を出力する
+        print_r($product_data);
+      ?>  
+    </p>
+  </body>
+</html>
