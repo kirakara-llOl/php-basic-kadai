0e180c508dcf8b53f4e60af3c32f406103232796
kirakara-llOl <goto.kn.my@gmail.com>
2024-03-17 17:07:03 +0900

    kadai_011ディレクトリの追加

---
+++ kadai_011/foreach.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>【課題】foreach文を使って連想配列の値とキーを出力しよう</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        $product_data = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道'];
+        foreach ($product_data as $key => $value) {
+          echo "{$key}：{$value}<br>";
+        }
+      ?>
+    </p>
+  </body>
+</html>
